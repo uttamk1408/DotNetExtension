a8a1d70f98d1229b024685f546ff6c9eadd5d58b
uttamk1408 <uttamkumar@Uttams-MacBook-Pro.local>
2022-02-26 16:47:03 +0530

    Adding extention solution

---
+++ partialclass/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+CalculateClass objCC = new CalculateClass();
+int i = 5;
+int j = 10;
+
+objCC.Add(i, j);
+objCC.Multiply(i, j);
+
+
+public partial class CalculateClass
+{
+    public void Add(int i, int j)
+    {
+        Console.WriteLine("Total: " + i + j);
+    }
+}
+
+public partial class CalculateClass
+{
+    public void Multiply(int i, int j)
+    {
+        Console.WriteLine("Total: " + i * j);
+    }
+}
